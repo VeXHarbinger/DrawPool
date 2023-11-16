59b808ebc006d466df82c4df59e9a5a4dec5a394
Alex Pearlstein <alexpearlstein@gmail.com>
2023-11-16 12:08:02 -0500

    - Updated references. - Fixed Settings Layout

---
+++ DrawPool/Logic/StringTools.cs
@@ -0,0 +1,17 @@
+﻿using WPFLocalizeExtension.Engine;
+
+namespace DrawPool.Logic
+{
+    /// <summary>
+    /// General String manipulation Tools
+    /// </summary>
+    public class StringTools
+    {
+        /// <summary>
+        /// Gets the localized version of the given string.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>The localized string</returns>
+        public static string GetLocalized(string key) => LocalizeDictionary.Instance.GetLocalizedObject("DrawPool", "Strings", key, LocalizeDictionary.Instance.Culture)?.ToString();
+    }
+}
